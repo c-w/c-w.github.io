e4070240b40642b93529d5a97fb6a91fa4712c55
Clemens Wolff <clewolff@microsoft.com>
2019-01-09 15:50:49 -0500

    Ensure to only deploy necessary files

--- .travis/commit-and-push.sh
+++ .travis/commit-and-push.sh
@@ -5,7 +5,14 @@ if [ -z "${GITHUB_TOKEN}" ]; then
   exit 1
 fi
 
-if git commit --message "Travis build ${TRAVIS_BUILD_NUMBER}" --author "Travis CI <travis@travis-ci.org>"; then
-  git remote add upstream "https://${GITHUB_TOKEN}@github.com/c-w/c-w.github.io.git"
-  git push upstream "HEAD:${TRAVIS_BRANCH}"
-fi
+git config --global user.name "Travis CI"
+git config --global user.email "travis@travis-ci.org"
+
+git checkout --orphan gh-pages
+git ls-files -z | xargs -0 git rm --cached --force
+
+git add github.svg favicon.ico index.html CNAME
+git commit --message "Travis build ${TRAVIS_BUILD_NUMBER}"
+
+git remote add upstream "https://${GITHUB_TOKEN}@github.com/c-w/c-w.github.io.git"
+git push --force upstream gh-pages:master
--- .travis/fetch-github-chart.sh
+++ .travis/fetch-github-chart.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-if curl --silent --fail "https://ghchart.rshah.org/c-w" > github.svg; then
-  git add github.svg
-fi
+curl --silent --fail "https://ghchart.rshah.org/c-w" > github.svg
