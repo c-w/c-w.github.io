724435e5cfdfd426c08bd520590e73a84be018e4
Clemens Wolff <clewolff@microsoft.com>
2019-01-09 15:19:57 -0500

    Specify which local revision to push to Github

--- .travis/commit-and-push.sh
+++ .travis/commit-and-push.sh
@@ -7,5 +7,5 @@ fi
 
 if git commit --message "Travis build ${TRAVIS_BUILD_NUMBER}" --author "Travis CI <travis@travis-ci.org>"; then
   git remote add upstream "https://${GITHUB_TOKEN}@github.com/c-w/c-w.github.io.git"
-  git push upstream "${TRAVIS_BRANCH}"
+  git push upstream "HEAD:${TRAVIS_BRANCH}"
 fi
