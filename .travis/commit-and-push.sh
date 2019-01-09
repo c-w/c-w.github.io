d9d04da10673579dab02278198e0be5013547cd3
Clemens Wolff <clewolff@microsoft.com>
2019-01-09 15:11:49 -0500

    Check for required environment variables

--- .travis/commit-and-push.sh
+++ .travis/commit-and-push.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z "${GITHUB_TOKEN}" ]; then
+  echo "Missing GITHUB_TOKEN environment variable" >&2
+  exit 1
+fi
+
 if git commit --message "Travis build ${TRAVIS_BUILD_NUMBER}" --author "Travis CI <travis@travis-ci.org>"; then
   git remote add upstream "https://${GITHUB_TOKEN}@github.com/c-w/c-w.github.io.git"
   git push upstream "${TRAVIS_BRANCH}"
